fa092cc78ac6889faa36fb86da75e093a6dce01e
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-03-29 10:26:53 +0000

    Close Menu on Link click

--- src/components/headerComps/MenuSheet.tsx
+++ src/components/headerComps/MenuSheet.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Sheet,
   SheetClose,
@@ -8,12 +10,16 @@ import { AlignJustify, X } from "lucide-react";
 import Image from "next/image";
 import logo from "../../../public/logo.png";
 import { links } from "./Header";
+import { useState } from "react";
 import NavigationLink from "./NavigationLink";
 
 
 export default function MenuSheet() {
+  
+  const [open, setOpen] = useState(false)
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger className="md:hidden">
         <AlignJustify />
       </SheetTrigger>
@@ -32,9 +38,9 @@ export default function MenuSheet() {
           </SheetClose>
         </div>
 
-        <div className="mt-12  flex flex-col gap-y-6 justify-center  ">
-          {links.map((link) => (
-            <NavigationLink key={link} link={link} />
+        <div className="mt-12  flex flex-col gap-y-6 justify-center text-center  ">
+          {links.map((link) => ( 
+            <NavigationLink key={link} link={link} setOpen={setOpen} />
           ))}
         </div>
       </SheetContent>
--- src/components/headerComps/NavigationLink.tsx
+++ src/components/headerComps/NavigationLink.tsx
@@ -2,12 +2,22 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Dispatch, SetStateAction } from "react";
 
-export default function NavigationLink({link}: {link:string}) {
+type propsType = {
+  link:string
+  setOpen?:Dispatch<SetStateAction<boolean>>
+}
+
+export default function NavigationLink({link,setOpen}: propsType) {
   const pathname = usePathname()
 
+  const closeMenu = ()=> {
+    setOpen && setOpen(false)
+  }
+
   return (
-    <Link href={`/${link !== 'Home' ? link.toLowerCase() : ''}`} key={link} className={` text-[15px] hover:text-neutral-800 hover:underline underline-offset-2 
+    <Link href={`/${link !== 'Home' ? link.toLowerCase() : ''}`} onClick={closeMenu} key={link} className={` text-[15px] hover:text-neutral-800 hover:underline underline-offset-2 
 
     ${ (pathname.includes(link.toLowerCase()) || (pathname === '/' && link === 'Home')) ? 'text-neutral-800 underline' : 'text-neutral-600' }
 
